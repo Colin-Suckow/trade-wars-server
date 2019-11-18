e56935f66848b7448e8506dfeef0871569745be8
Colin Suckow <colin@suckow.dev>
2019-11-18 10:57:33 -0800

    Add redirect to client function

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world"))
 }
 
+func redirectToClient(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://carson-key.github.io/trade-wars-static/#/", 308)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/redirect", redirectToClient)
 
 	log.Println("Starting server on port 4000")
 	err := http.ListenAndServe(":4000", mux)
