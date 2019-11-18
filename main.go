a96a35d5926ef116a679a7af8b852ff01ab15931
Colin Suckow <colin@suckow.dev>
2019-11-18 11:04:44 -0800

    Tie redirects to routes

--- main.go
+++ main.go
@@ -9,14 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world"))
 }
 
-func redirectToClient(w http.ResponseWriter, r *http.Request) {
+func clientPlayersScreen(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "https://carson-key.github.io/trade-wars-static/#/", 308)
+}
+
+func clientGameScreen(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://carson-key.github.io/trade-wars-static/#/game", 308)
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/redirect", redirectToClient)
+	mux.HandleFunc("/players", clientPlayersScreen)
+	mux.HandleFunc("/map", clientGameScreen)
 
 	log.Println("Starting server on port 4000")
 	err := http.ListenAndServe(":4000", mux)
