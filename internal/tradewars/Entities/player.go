07c5e85d2fe7a8295684b213e454c6d5a3a81a28
Colin Suckow <colin@suckow.dev>
2019-11-25 18:12:29 -0800

    Add relevent components to player

--- internal/tradewars/Entities/player.go
+++ internal/tradewars/Entities/player.go
@@ -2,6 +2,10 @@ package tradewars
 
 import "suckow.dev/trade-wars-server/internal/ecs"
 
+import tradewars "suckow.dev/trade-wars-server/internal/tradewars/Components"
+
 type Player struct {
 	ecs.BasicEntity
+	tradewars.NameComponent
+	tradewars.PositionComponent
 }
