a47d5614f58a0123e567ea9d5467263e18591c45
Colin Suckow <csuckow99@gmail.com>
2019-11-25 18:34:58 -0800

    Add parents and children to Basic entity

--- internal/ecs/basicEntity.go
+++ internal/ecs/basicEntity.go
@@ -3,7 +3,9 @@ package ecs
 import "math/rand"
 
 type BasicEntity struct {
-	id uint64
+	id            uint64
+	parentEntity  BasicEntity
+	childEntities []BasicEntity
 }
 
 func makeBasicEntity() BasicEntity {
