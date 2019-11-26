bfee8b21c3c889aaaf9265bfc02905dee364ce85
Colin Suckow <csuckow99@gmail.com>
2019-11-25 19:32:27 -0800

    Add callsign to ownership for long term ownership

--- internal/tradewars/Components/ownership.go
+++ internal/tradewars/Components/ownership.go
@@ -1,7 +1,9 @@
 package tradewars
 
 //Can be owned by a specific session
+//Callsign for persistant ownership, sessionId for validating requests
 
 type OwnershipComponent struct {
+	callsign  string
 	sessionId int64
 }
