4a68e6b7475852e79e72e852d2b32881c8e47e39
Colin Suckow <colin@suckow.dev>
2019-11-26 10:44:37 -0800

    Allow more cors methods

--- internal/networking/cors.go
+++ internal/networking/cors.go
@@ -5,4 +5,6 @@ import "net/http"
 //Allow any client to access api. Begone cors
 func EnableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
