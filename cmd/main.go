14e14c03321e969082b49e6ed6409185fb72e0ce
Colin Suckow <csuckow99@gmail.com>
2019-11-25 20:17:35 -0800

    Move routes to different package. RLS errors ¯\_(ツ)_/¯

--- cmd/main.go
+++ cmd/main.go
@@ -4,25 +4,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	networking "suckow.dev/trade-wars-server/internal/networking"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func handleHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world"))
-}
-
-func clientPlayersScreen(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://carson-key.github.io/trade-wars-static/#/", 308)
-}
-
-func clientGameScreen(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://carson-key.github.io/trade-wars-static/#/game", 308)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", clientPlayersScreen)
-	mux.HandleFunc("/players", clientPlayersScreen)
-	mux.HandleFunc("/map", clientGameScreen)
+	mux.HandleFunc("/", handleHome)
+	mux.HandleFunc("/players", networking.ClientPlayersScreen)
+	mux.HandleFunc("/map", networking.ClientGameScreen)
 
 	portVariable := os.Getenv("PORT")
 	if portVariable == "" {
---
+++ internal/networking/redirects.go
@@ -0,0 +1,11 @@
+package networking
+
+import "net/http"
+
+func ClientPlayersScreen(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://carson-key.github.io/trade-wars-static/#/", 308)
+}
+
+func ClientGameScreen(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://carson-key.github.io/trade-wars-static/#/game", 308)
+}
