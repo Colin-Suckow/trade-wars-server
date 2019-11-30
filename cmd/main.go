a4808886e580a84c2e6a4a3fdf07f22b61db1bfc
Colin Suckow <csuckow99@gmail.com>
2019-11-29 17:56:11 -0800

    Start map system

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,12 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
-	"log"
 	"net/http"
-	"os"
-	networking "suckow.dev/trade-wars-server/internal/networking"
+	"suckow.dev/trade-wars-server/internal/networking"
 )
-
-func testReturnCallsign(w http.ResponseWriter, r *http.Request) {
-	networking.EnableCors(w)
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	var cookie, err = r.Cookie("callsign")
-	if err != nil {
-		http.Error(w, "Couldn't read callsign. :(", 500)
-		return
-	}
-	callsign := cookie.Value
-	w.Write([]byte("Hello " + callsign))
-}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", networking.ClientPlayersScreen)
-	mux.HandleFunc("/players", networking.ClientPlayersScreen)
-	mux.HandleFunc("/map", networking.ClientGameScreen)
-	mux.HandleFunc("/startSession", networking.IssueCookie)
-	mux.HandleFunc("/getCallsign", testReturnCallsign)
-
-	err := godotenv.Load()
-	portVariable := os.Getenv("PORT")
-
-	log.Println("Starting server on port " + portVariable)
-	err = http.ListenAndServe(":"+portVariable, mux)
-	log.Fatal(err)
+	networking.SetupRoutes(mux)
+	networking.ServeMux(mux)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/EngoEngine/ecs v1.0.3
-	github.com/asaskevich/EventBus v0.0.0-20180315140547-d46933a94f05 // indirect
+	github.com/asaskevich/EventBus v0.0.0-20180315140547-d46933a94f05
 	github.com/joho/godotenv v1.3.0
 )
